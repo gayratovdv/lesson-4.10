975ee6e3b4d29fc2355e26c28e7b9745f0973325
javohirgvdev <gayratovjavohir90@gmail.com>
2024-04-05 22:48:36 +0500

    I added js and html

---
+++ script.js
@@ -0,0 +1,10 @@
+let random = prompt("Birinchi sonni kiriting")
+let random2 = prompt("Ikkinchi sonni kiriting")
+
+function upper () {
+    if (random < random2) {
+        alert( "eng katta son bu: " + random2)
+    }else(alert( "eng katta son bu: " + random))
+}
+
+upper();
